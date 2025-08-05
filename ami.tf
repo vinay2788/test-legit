3e46413c0c1db83610a7b313ea656790386fe2e7
vinay2788 <vinaybali121@gmail.com>
2025-08-06 02:50:13 +0530

    terra

--- ami.tf
+++ ami.tf
@@ -1,9 +1,6 @@
 # A Terraform file to test the approved_ami.rego custom rule
 # See our blog post for details: https://blog.fugue.co
 
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_instance" "good" {
   ami           = "ami-09e67e426f25ce0d7"
